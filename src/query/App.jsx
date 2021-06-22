304ed3fd44ad56884ec9a7537770b755f5cc5c58
muffinfeng <1697426994@qq.com>
2021-06-22 22:36:17 +0800

    9.3

--- src/query/App.jsx
+++ src/query/App.jsx
@@ -1,13 +1,153 @@
 import { connect } from 'react-redux';
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import './App.css';
 import Nav from '../common/Nav.jsx';
 import List from './List.jsx';
 import Bottom from './Bottom.jsx';
+import Header from '../common/Header';
+import URI from 'urijs';
+import { h0 } from '../common/fp';
+import dayjs from 'dayjs';
+
+import {
+    setFrom,
+    setTo,
+    setDepartDate,
+    setHighSpeed,
+    setSearchParsed,
+    setTrainList,
+    setTicketTypes,
+    setTrainTypes,
+    setDepartStations,
+    setArriveStations,
+} from './actions';
 
 function App(props) {
+    const {
+        trainList,
+        from,
+        to,
+        departDate,
+        highSpeed,
+        searchParsed,
+        dispatch,
+        orderType,
+        onlyTickets,
+        isFiltersVisible,
+        ticketTypes,
+        trainTypes,
+        departStations,
+        arriveStations,
+        checkedTicketTypes,
+        checkedTrainTypes,
+        checkedDepartStations,
+        checkedArriveStations,
+        departTimeStart,
+        departTimeEnd,
+        arriveTimeStart,
+        arriveTimeEnd,
+    } = props;
+
+    const back = useCallback(() => {
+        window.history.back();
+    }, []);
+
+    useEffect(() => {
+        const queries = URI.parseQuery(window.location.search);
+
+        const { from, to, date, highSpeed } = queries;
+
+        dispatch(setFrom(from));
+        dispatch(setTo(to));
+        dispatch(setDepartDate(h0(dayjs(date)).valueOf()));
+        dispatch(setHighSpeed(highSpeed === 'true'));
+
+        dispatch(setSearchParsed(true));
+    }, []);
+
+    useEffect(() => {
+        if (!searchParsed) {
+            return;
+        }
+        const url = new URI('/rest/query')
+            .setSearch('from', from)
+            .setSearch('to', to)
+            .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
+            .setSearch('highSpeed', highSpeed)
+            .setSearch('orderType', orderType)
+            .setSearch('onlyTickets', onlyTickets)
+            .setSearch(
+                'checkedTicketTypes',
+                Object.keys(checkedTicketTypes).join()
+            )
+            .setSearch(
+                'checkedTrainTypes',
+                Object.keys(checkedTrainTypes).join()
+            )
+            .setSearch(
+                'checkedDepartStations',
+                Object.keys(checkedDepartStations).join()
+            )
+            .setSearch(
+                'checkedArriveStations',
+                Object.keys(checkedArriveStations).join()
+            )
+            .setSearch('departTimeStart', departTimeStart)
+            .setSearch('departTimeEnd', departTimeEnd)
+            .setSearch('arriveTimeStart', arriveTimeStart)
+            .setSearch('arriveTimeEnd', arriveTimeEnd)
+            .toString();
+
+        fetch(url)
+            .then(result => result.json())
+            .then(json => {
+                const {
+                    dataMap: {
+                        directTrainInfo: {
+                            trains,
+                            filter: {
+                                ticketType,
+                                trainType,
+                                depStation,
+                                arrStation,
+                            },
+                        },
+                    },
+                } = json;
+
+                dispatch(setTrainList(trains));
+                dispatch(setTicketTypes(ticketType));
+                dispatch(setTrainTypes(trainType));
+                dispatch(setDepartStations(depStation));
+                dispatch(setArriveStations(arrStation));
+            });
+    }, [
+        from,
+        to,
+        departDate,
+        highSpeed,
+        searchParsed,
+        orderType,
+        onlyTickets,
+        checkedTicketTypes,
+        checkedTrainTypes,
+        checkedDepartStations,
+        checkedArriveStations,
+        departTimeStart,
+        departTimeEnd,
+        arriveTimeStart,
+        arriveTimeEnd,
+    ]);
+
+    if (!searchParsed) {
+        return null;
+    }
+
     return (
         <div>
+            <div className="header-wrapper">
+                <Header title={`${from} ⇀ ${to}`} onBack={back} />
+            </div>
             <Nav />
             <List />
             <Bottom />
